f8be88407ed7a4a7b4da9cd770281de3a054b5ff
Rasim <sadikoglurasim@gmail.com>
2021-05-26 14:16:14 +0300

    dpi

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include <vector>
 #include <map>
 #include <chrono>
+#include <stack>
 
 using std::vector;
 using std::map;
 
 void knapsack_solve(const vector<vector<int>> &items, const vector<int> &limits);
 int knapsack_dp(const vector<vector<int>> &items, map<vector<int>, int> &table, vector<int> &weights);
+int knapsack_dpi(const vector<vector<int>> &items, map<vector<int>, int> &table, vector<int> weights);
 
 int main(int argc, char **argv) {
 
@@ -69,13 +71,14 @@ void knapsack_solve(const vector<vector<int>> &items, const vector<int> &limits)
     weights.push_back(items.size() - 1);
 
     vector<int> res_items;
-    int result = knapsack_dp(items, table, weights);
+    int result = knapsack_dpi(items, table, weights);
 
     std::cout << result << std::endl;
 
     return;
 }
 
+// Recursive DP
 int knapsack_dp(const vector<vector<int>> &items, map<vector<int>, int> &table, vector<int> &weights) {
 
     int *val, item;
@@ -111,3 +114,55 @@ int knapsack_dp(const vector<vector<int>> &items, map<vector<int>, int> &table, 
 
     return *val;
 }
+
+// Iterative DP
+int knapsack_dpi(const vector<vector<int>> &items, map<vector<int>, int> &table, vector<int> weights) {
+    
+    std::stack<vector<int>> s;
+    s.push(weights);
+    do {
+        vector<int> current = s.top();
+
+        current[current.size() - 1]--;
+
+        int v1 = 0, v2 = 0;
+        if (current.back() == -1) {
+            v1 = 0;
+        } else if ((v1 = table[current]) == 0) {
+            s.push(current);
+            continue;
+        }
+
+        bool isv2 = true;
+        for (size_t i = 0; i < current.size() - 1; i++) {
+            current[i] -= items[current.back() + 1][i + 1];
+            if (current[i] < 0) {
+                isv2 = false;
+                break;
+            }
+        }
+        
+        if (isv2 && current.back() == -1) {
+            v2 = 0;
+        } else if (isv2 && (v2 = table[current]) == 0) {
+            s.push(current);
+            continue;
+        }
+
+        if (v1 == -1) v1 = 0;
+        if (v2 == -1) v2 = 0;
+
+        if (isv2) v2 += items[current.back() + 1][0];
+
+        int *val = &table[s.top()];
+        *val = v1;
+        if (isv2) *val = v2 > v1 ? v2 : v1;
+        if (*val == 0) *val = -1;
+
+        s.pop();
+
+    } while (!s.empty());
+
+    return table[weights];
+}
+
