cd6fb5d915bbb363b89ae2882491c2b791380207
Rasim <sadikoglurasim@gmail.com>
2021-06-12 21:41:53 +0300

    .

--- main.cpp
+++ main.cpp
@@ -17,11 +17,13 @@ Rasim Sadıkoğlu 150118009
 using std::vector;
 using std::map;
 
-void knapsack_solve(const vector<vector<int>> &items, const vector<int> &limits);
+void knapsack_solve(vector<vector<int>> items, const vector<int> &limits);
 int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_stack[]);
+int knapsack_dfs_mirror(vector<vector<int>> items, const vector<int> &limits, bool max_stack[]);
+void greedy_sort(vector<vector<int>> &items, const vector<int> &limits);
 bool sortf(vector<int> v1, vector<int> v2);
 void handler(int num);
-void print_console(int stage, int max, int max_stage);
+void print_console(std::string s, int stage, int max, int max_stage);
 
 // For signal handler, signal handler will make this true whenever user presses CTRL+C.
 volatile bool terminate = false;
@@ -91,11 +93,25 @@ int main(int argc, char **argv) {
 }
 
 // Helper function for the real algorithm.
-void knapsack_solve(const vector<vector<int>> &items, const vector<int> &limits) {
+void knapsack_solve(vector<vector<int>> items, const vector<int> &limits) {
+
+    greedy_sort(items, limits);
+
+    vector<int> weights(limits.size());
+    size_t i;
+    bool isfit = true;
+    for (i = items.size() - 1; i != -1 && isfit; i--) {
+        for (size_t j = 0; j < limits.size() && isfit; j++) {
+            isfit = (weights[j] += items[i][j + 1]) < limits[j];
+        }
+    }
+    i = items.size() - i;
 
     // Create a boolean array for result.
     bool max_stack[items.size()] {};
-    int result = knapsack_dfs(items, limits, max_stack);
+    int result;
+    if (i < items.size() / 2) result = knapsack_dfs(items, limits, max_stack);
+    else result = knapsack_dfs_mirror(items, limits, max_stack);
 
     // Print the results to the stdout.
     std::cout << result << std::endl;
@@ -112,23 +128,6 @@ void knapsack_solve(const vector<vector<int>> &items, const vector<int> &limits)
 }
 
 int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_stack[]) {
-
-    // Sort the array by items' greediness value.
-    for (size_t i = 0; i < items.size(); i++) {
-        float val = 0;
-        for (size_t j = 0; j < limits.size(); j++) {
-            val += (float)items[i][j + 1] / limits[j];
-        }
-        val /= limits.size();
-        val = items[i][0] / val;
-        items[i].push_back(val);
-    }
-
-    // Add items' index for restoring original positions 
-    // in order to find which items are included in the solution.
-    for (size_t i = 0; i < items.size(); i++) items[i].push_back(i);
-    std::sort(items.begin(), items.end(), sortf);
-
     // Save number of items and number of knapsacks for easy access.
     int limits_size = limits.size();
     int items_size = items.size();
@@ -164,7 +163,7 @@ int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_
             // Check for stage.
             if (si < si_min) {
                 si_min = si;
-                print_console(si_min, max_value, items_size);
+                print_console("DFS", items_size - si_min, max_value, items_size);
             }
 
             prev = current;
@@ -186,6 +185,7 @@ int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_
 
         // If It can't fit into the knapsack...
         if (!isfit) {
+            //terminate = true;
             // Update si according to prev.
             if (!prev) si = items_size - 1;
             else si--;
@@ -203,7 +203,7 @@ int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_
         if (value > max_value) {
             max_value = value;
             memcpy(max_stack, stack, items_size * sizeof(bool));
-            print_console(si_min, max_value, items_size);
+            print_console("DFS", items_size - si_min, max_value, items_size);
         }
 
         // Update si according to prev.
@@ -222,6 +222,110 @@ int knapsack_dfs(vector<vector<int>> items, const vector<int> &limits, bool max_
     return max_value;
 }
 
+int knapsack_dfs_mirror(vector<vector<int>> items, const vector<int> &limits, bool max_stack[]) {
+    int limits_size = limits.size();
+    int items_size = items.size();
+
+    int weights[limits_size] {};
+
+    int max_value = 0;
+
+    bool stack[items_size] {};
+    for (int i = 0; i < items_size; i++) stack[i] = 1;
+    int si = 0;
+
+    int si_max = 0;
+
+    int value = 0;
+    bool prev = 0;
+
+    for (int i = 0; i < items_size; i++) {
+        value += items[i][0];
+        for (int j = 0; j < limits_size; j++) {
+            weights[j] += items[i][j + 1];
+        }
+    }
+
+    while (si < items_size && !terminate) {
+
+        bool current = stack[si];
+
+        if (!current) {
+            value += items[si][0];
+            for (int i = 0; i < limits_size; i++) weights[i] += items[si][i + 1];
+
+            if (si > si_max) {
+                si_max = si;
+                print_console("Mirror DFS", si_max + 1, max_value, items_size);
+            }
+
+            prev = current;
+            stack[si] = 1;
+            si++;
+            continue;
+        }
+
+        int tw[limits_size];
+        memcpy(tw, weights, sizeof(int) * limits_size);
+
+        bool isfit = true;
+        for (int i = 0; i < limits_size; i++) {
+            tw[i] -= items[si][i + 1];
+            if (isfit) isfit = tw[i] <= limits[i];
+        }
+
+        if (isfit) {
+            stack[si] = 0;
+            value -= items[si][0];
+            if (value > max_value) {
+                max_value = value;
+                memcpy(max_stack, stack, items_size * sizeof(bool));
+                print_console("Mirror DFS", si_max + 1, max_value, items_size);
+            }
+            value += items[si][0];
+            stack[si] = 1;
+            if (prev) si = 0;
+            else si++;
+            prev = 0;
+            continue;
+        }
+
+        stack[si] = 0;
+
+        value -= items[si][0];
+        memcpy(weights, tw, sizeof(int) * limits_size);
+
+        if (!prev) si = 0;
+        else si++;
+
+        prev = current;
+    }
+
+    for (size_t i = 0; i < items.size(); i++) items[i].push_back(max_stack[i]);
+
+    std::sort(items.begin(), items.end(), sortf);
+    for (int i = 0; i < items_size; i++) max_stack[i] = items[i].back();
+
+    return max_value;
+}
+
+void greedy_sort(vector<vector<int>> &items, const vector<int> &limits) {
+    for (size_t i = 0; i < items.size(); i++) {
+        float val = 0;
+        for (size_t j = 0; j < limits.size(); j++) {
+            val += (float)items[i][j + 1] / limits[j];
+        }
+        val /= limits.size();
+        val = items[i][0] / val;
+        items[i].push_back(val);
+    }
+
+    for (size_t i = 0; i < items.size(); i++) items[i].push_back(i);
+    std::sort(items.begin(), items.end(), sortf);
+
+    return;
+}
+
 // Function for sort the vector.
 bool sortf(vector<int> v1, vector<int> v2) {
     return v1[v1.size() - 2] < v2[v2.size() - 2];
@@ -234,7 +338,7 @@ void handler(int num) {
     return;
 }
 
-void print_console(int stage, int max, int max_stage) {
+void print_console(std::string s, int stage, int max, int max_stage) {
 
     #if unix || __unix || __unix__
     system("clear");
@@ -242,7 +346,8 @@ void print_console(int stage, int max, int max_stage) {
     system("cls");
     #endif
     
-    std::cout << "Current Stage: " << max_stage - stage << "/" << max_stage << std::endl;
+    std::cout << s << "\n";
+    std::cout << "Current Stage: " << stage << "/" << max_stage << std::endl;
     std::cout << "Current Maximum Value: " << max << std::endl;
     std::cout << "Pressing CTRL+C will save current results." << std::endl;
 
